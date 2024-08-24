886c9ac925d185305b3c81bbe0c413e90e2d4870
AdministratorX2p <anirbanuchiha76@gmail.com>
2024-08-24 15:29:58 +0530

    Update userbot.py

--- BrandrdXMusic/core/userbot.py
+++ BrandrdXMusic/core/userbot.py
@@ -71,10 +71,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("ABOUT_BRANDEDKING")
+                await self.one.join_chat("YorXsupport")
+                await self.one.join_chat("YorXUpdate")
+                await self.one.join_chat("ThunderBoltFantasy")
+                await self.one.join_chat("None")
 
             except:
                 pass
@@ -101,10 +101,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.two.join_chat("BRANDED_PAID_CC")
-                await self.two.join_chat("BRANDRD_BOT")
-                await self.two.join_chat("ABOUT_BRANDEDKING")
+                await self.two.join_chat("YorXsupport")
+                await self.two.join_chat("YorXUpdate")
+                await self.two.join_chat("ThunderBoltFantasy")
+                await self.two.join_chat("None")
             except:
                 pass
             assistants.append(2)
@@ -125,10 +125,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BRANDED_WORLD")
-                await self.three.join_chat("BRANDED_PAID_CC")
-                await self.three.join_chat("BRANDRD_BOT")
-                await self.three.join_chat("ABOUT_BRANDEDKING")
+                await self.three.join_chat("YorXsupport")
+                await self.three.join_chat("YorXUpdate")
+                await self.three.join_chat("ThunderboltFantasy")
+                await self.three.join_chat("None")
             except:
                 pass
             assistants.append(3)
@@ -148,10 +148,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BRANDED_WORLD")
-                await self.four.join_chat("BRANDED_PAID_CC")
-                await self.four.join_chat("BRANDRD_BOT")
-                await self.four.join_chat("ABOUT_BRANDEDKING")
+                await self.four.join_chat("YorXsupport")
+                await self.four.join_chat("YorXUpdate")
+                await self.four.join_chat("ThunderboltFantasy")
+                await self.four.join_chat("None")
             except:
                 pass
             assistants.append(4)
