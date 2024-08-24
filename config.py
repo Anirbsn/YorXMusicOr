a6379ba3a923987658904708e683b2df48788fb6
AdministratorX2p <anirbanuchiha76@gmail.com>
2024-08-24 15:44:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/YorXSupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/YorXUpdate")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/YorXsupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
