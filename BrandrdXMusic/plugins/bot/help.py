7223f765ec9daad5e18a9c165b8b517fe94c04a8
AdministratorX2p <anirbanuchiha76@gmail.com>
2024-08-24 13:01:36 +0530

    Update help.py

--- BrandrdXMusic/plugins/bot/help.py
+++ BrandrdXMusic/plugins/bot/help.py
@@ -49,7 +49,7 @@ async def helper_private(
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
     await message.reply_video(
-        video="https://te.legra.ph/file/51293513e6af319726fe7.mp4",
+        video="https://telegra.ph/file/89fde66471d370e99f217.mp4",
         caption=_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
